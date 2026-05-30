ef8f730bd659d54fa83c03a5c90366f3312108d4
JackieWongz <2537020964@qq.com>
2026-05-30 14:36:13 +0800

    chore: update build configuration to include new vite entry and enhance exports

--- packages/infcss/src/index.ts
+++ packages/infcss/src/index.ts
@@ -1,2 +1,11 @@
 export * from '@infcss/core'
-export * from '@infcss/vite'
+export * from '@infcss/shared'
+export * from '@infcss/presets'
+
+import { rules, variants } from '@infcss/presets'
+
+export { useInfCSS } from '@infcss/core'
+export type { Rule, Variant, VariantItem, InfCSSConfig } from '@infcss/shared'
+export { defineInfCSSConfig } from '@infcss/shared'
+
+export { rules as defaultRules, variants as defaultVariants }
